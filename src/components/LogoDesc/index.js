007f279abd4e064155aaf464f45c0209482f5717
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-05-04 10:29:51 +0100

    feat(adding-light-font): adding font light

--- src/components/LogoDesc/index.js
+++ src/components/LogoDesc/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const LogoDesc = ({ logo, desc, fontName }) => (
+const LogoDesc = ({ logo, desc }) => (
     <div id="LogoDesc" className="logoDescContainer">
     <img className="box" src={logo}></img>
     <div
